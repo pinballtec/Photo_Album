4fa758542aacea78c977d145e4491aa85e79b408
pinballtec <daniilboiko99@gmail.com>
2022-03-14 14:42:48 +0100

    Gallery Template completed

--- photoalbum/photoalbum_app/urls.py
+++ photoalbum/photoalbum_app/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.test, name='test')
+    path('', views.main, name='main_page'),
+    path('add/', views.add_photo, name='add_photo'),
+    path('view/<int:pk>', views.view_photo, name='view_photo'),
 ]
--- photoalbum/photoalbum_app/views.py
+++ photoalbum/photoalbum_app/views.py
@@ -3,5 +3,13 @@ from django.http import HttpResponse
 # Create your views here.
 
 
-def test(response):
-    return HttpResponse('Test')
+def main(request):
+    return render(request, 'photoalbum_app/index.html')
+
+
+def add_photo(request):
+    return render(request, 'photoalbum_app/add.html')
+
+
+def view_photo(request, pk):
+    return render(request, 'photoalbum_app/photo.html')
