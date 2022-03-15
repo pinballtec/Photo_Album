553073f0563cf892c227717126ea2e2d8e1c2168
pinballtec <daniilboiko99@gmail.com>
2022-03-15 20:29:27 +0100

    all data is rendered on page

--- photoalbum/photoalbum_app/views.py
+++ photoalbum/photoalbum_app/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
+from .models import Category, Photo
 from django.http import HttpResponse
 # Create your views here.
 
 
 def main(request):
-    return render(request, 'photoalbum_app/index.html')
+    category = Category.objects.all()
+    photos = Photo.objects.all()
+    context = {'categories': category, 'photos': photos}
+    return render(request, 'photoalbum_app/index.html', context)
 
 
 def add_photo(request):
@@ -12,4 +16,5 @@ def add_photo(request):
 
 
 def view_photo(request, pk):
-    return render(request, 'photoalbum_app/photo.html')
+    photo = Photo.objects.get(id=pk)
+    return render(request, 'photoalbum_app/photo.html', {'photo': photo})
