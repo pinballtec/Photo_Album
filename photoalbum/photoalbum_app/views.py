0ccbeb78d461499a34a5397a1ea5826ae2467737
pinballtec <daniilboiko99@gmail.com>
2022-03-16 14:12:37 +0100

    add function and add page is completed

--- photoalbum/photoalbum_app/views.py
+++ photoalbum/photoalbum_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Category, Photo
 from django.http import HttpResponse
 # Create your views here.
@@ -12,7 +12,26 @@ def main(request):
 
 
 def add_photo(request):
-    return render(request, 'photoalbum_app/add.html')
+    category = Category.objects.all()
+    if request.method == 'POST':
+        data = request.POST
+        image = request.FILES.get('image')
+        if data['category'] != 'none':
+            category = Category.objects.get(id=data['category'])
+        elif data['category_new'] != '':
+            category, created = Category.objects.get_or_create(
+                name=data['category_new'])
+        else:
+            category = None
+        photo = Photo.objects.create(
+            category=category,
+            description=data['description'],
+            image=image,
+        )
+        return redirect('main_page')
+
+    context = {'categories': category}
+    return render(request, 'photoalbum_app/add.html', context)
 
 
 def view_photo(request, pk):
